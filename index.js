ffecf8ad06a56f439c9f12cb67fff46cad7837e9
Thomas Dürr <thomduerr@gmail.com>
2024-06-22 13:44:16 +0200

    options

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ import { visit } from 'unist-util-visit'
 import { toString } from 'mdast-util-to-string'
 import { findAfter } from 'unist-util-find-after'
 
-const remarkHeadlineRanges = () => {
+const remarkHeadlineRanges = (opts) => {
+    const defaultOptions = {
+        maxDepth: 6,
+        urlPattern: 'edit/{start}/{end}',
+        linkText: 'Edit'
+    }
+    const options = { ...defaultOptions, ...opts }
     return (tree) => {
         const setHeadingSectionEnd = (node, index, parent, depth) => {
             let nextHeading = findAfter(parent, index, node => node.type == 'heading' && node.depth <= depth)
@@ -19,23 +25,28 @@ const remarkHeadlineRanges = () => {
             }
         }
         visit(tree, 'heading', (node, index, parent) => {
-            node.hierarchy = { start: {}, end: {} }
             let { depth } = node
-            node.hierarchy.start.line = node.position.start.line
-            setHeadingSectionEnd(node, index, parent, depth)
+            if (depth <= options.maxDepth) {
+                node.hierarchy = { start: {}, end: {} }
+                node.hierarchy.start.line = node.position.start.line
+                setHeadingSectionEnd(node, index, parent, depth)
+            }
         })
         visit(tree, 'heading', (node, index, parent) => {
-            const link = {
-                type: 'link',
-                url: `edit/${node.hierarchy.start.line}/${node.hierarchy.end.line}`,
-                children: [
-                    {
-                        type: 'text',
-                        value: '[ edit ]'
-                    }
-                ]
+            let { depth } = node
+            if (depth <= options.maxDepth) {
+                const link = {
+                    type: 'link',
+                    url: options.urlPattern.replace('{start}', node.hierarchy.start.line).replace('{end}', node.hierarchy.end.line),
+                    children: [
+                        {
+                            type: 'text',
+                            value: options.linkText
+                        }
+                    ]
+                }
+                node.children.splice(index + 1, 0, link)
             }
-            node.children.splice(index + 1, 0, link)
         })
     }
 }
